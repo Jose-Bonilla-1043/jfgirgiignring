9aa893a2de13acd96056cffe2af9480b7cfef023
Jose-Bonilla-1043 <164534053+Jose-Bonilla-1043@users.noreply.github.com>
2024-10-14 13:07:13 -0600

    Add files via upload

---
+++ grupo5i/alumno/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Alumno(models.Model):
+    apaterno=models.CharField(max_length=50,verbose_name="Apellido paterno")
+    amaterno=models.CharField(max_length=50,verbose_name="Apellido materno")
+    nombre=models.CharField(max_length=100,verbose_name="Nombre (s)")
+    fecha_nacimiento=models.DateField(verbose_name="Fecha de nacimiento ingreso",null=False,blank=False)
